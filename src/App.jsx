7949665bb94dd529268d7a1757fc756b9f404bfb
Louie <louie@lunardigitalassets.io>
2026-03-10 21:03:24 +0000

    Remove stripe dividers from Comparison section

--- src/App.jsx
+++ src/App.jsx
@@ -1141,12 +1141,8 @@ export default function AF() {
           </section>
         </div>
 
-        <div style={{ background:"#0F1D35", padding:"120px 0 0" }}><Stripe /></div>
-
         {/* COMPARISON TABLE */}
         <ComparisonSection />
-
-        <div style={{ background:"#0F1D35", padding:"0 0 120px" }}><Stripe flip /></div>
 
         {/* REVENUE SHARE */}
         <section ref={rR2} style={full}>
